10cf051663adca87edfc0969014d35ae5b02e2cd
Conor McFeely <cwmcfeely@gmail.com>
2024-01-15 20:16:20 +0000

    Adding the function to print game board and to take player input

--- tictactoe.py
+++ tictactoe.py
@@ -1,14 +1,26 @@
 # Creating the game board
-game_board = ["-","-","-",
-              "-","-","-",
-              "-","-","-",]
+gameBoard = ["-","-","-",
+             "-","-","-",
+             "-","-","-"]
 
-# Take player input
+# Some Global variables
+currentPlayer = 'X'
+winner = None
+gameRunning = True
 
-# Check if the move was a winning move
+# function to print the board to command line
+def printGameBoard(gameBoard):
+    print(" " + gameBoard[0] + " | " + gameBoard[1] + " | " + gameBoard[2])
+    print("-----------")
+    print(" " + gameBoard[3] + " | " + gameBoard[4] + " | " + gameBoard[5])
+    print("-----------")
+    print(" " + gameBoard[6] + " | " + gameBoard[7] + " | " + gameBoard[8])
+printGameBoard(gameBoard)
 
-# Take second player input
-
-# Check if the move was a winning move
-
-
+# Function to take players input
+def playerInput(gameBoard):
+    playerIn = int(input("Enter a number 1-9: ")) # Need to use int conversion as input outputs as string
+    if playerIn >= 1 and playerIn <=9 and gameBoard[playerIn-1] == "-":
+        gameBoard[playerIn-1] = currentPlayer
+    else:
+        print("That move has already been taken!")
