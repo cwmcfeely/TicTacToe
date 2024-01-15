fe29ebf8bcfca9f137e5d54e04adcdc37c6b9ad0
Conor McFeely <56093743+cwmcfeely@users.noreply.github.com>
2024-01-15 19:33:49 +0000

    First commit of the script change - creating the board

---
+++ tictactoe.py
@@ -0,0 +1,14 @@
+# Creating the game board
+game_board = ["-","-","-",
+              "-","-","-",
+              "-","-","-",]
+
+# Take player input
+
+# Check if the move was a winning move
+
+# Take second player input
+
+# Check if the move was a winning move
+
+
