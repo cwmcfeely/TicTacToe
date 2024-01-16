1415dd31e4ed0048ab6b7d656e0b0ff4106d3bbe
Conor McFeely <cwmcfeely@gmail.com>
2024-01-16 18:27:26 +0000

    Refactoring to follow the principles of PEP8 by renaming function names

--- tictactoe.py
+++ tictactoe.py
@@ -1,35 +1,37 @@
 import random
 
+print(" ")
 print("***********************")
 print("***** Tic-Tac-Toe *****")
 print("***********************")
+print(" ")
 # Creating the game board
-gameBoard = ["-","-","-",
+game_board = ["-","-","-",
              "-","-","-",
              "-","-","-"]
 
 # Some Global variables
-currentPlayer = 'X'
+current_player = 'X'
 winner = None
-gameRunning = True
+game_running = True
 
 # function to print the board to command line
-def printGameBoard(gameBoard):
+def print_game_board(game_board):
     print(" ")
-    print(" " + gameBoard[0] + " | " + gameBoard[1] + " | " + gameBoard[2])
+    print(" " + game_board[0] + " | " + game_board[1] + " | " + game_board[2])
     print("-----------")
-    print(" " + gameBoard[3] + " | " + gameBoard[4] + " | " + gameBoard[5])
+    print(" " + game_board[3] + " | " + game_board[4] + " | " + game_board[5])
     print("-----------")
-    print(" " + gameBoard[6] + " | " + gameBoard[7] + " | " + gameBoard[8])
+    print(" " + game_board[6] + " | " + game_board[7] + " | " + game_board[8])
     print(" ")
 
 # Function to take players input
-def playerInput(gameBoard):
+def player_input(game_board):
     try:
         while True:
-            playerIn = int(input(f"Player {currentPlayer} Enter a number 1-9: ")) # Need to use int conversion as input outputs as string
-            if playerIn >= 1 and playerIn <=9 and gameBoard[playerIn-1] == "-":
-                gameBoard[playerIn-1] = currentPlayer
+            playerIn = int(input(f"Player {current_player} Enter a number 1-9: ")) # Need to use int conversion as input outputs as string
+            if playerIn >= 1 and playerIn <=9 and game_board[playerIn-1] == "-":
+                game_board[playerIn-1] = current_player
                 break
             elif playerIn < 1 or playerIn > 9:
                 print("Please only enter a number betweem 1-9 and try again")
@@ -40,102 +42,102 @@ def playerInput(gameBoard):
 
 
 # Checking for horizontal win or tie
-def checkHorizontal(gameBoard):
+def check_horizontal(game_board):
     global winner # Global will change the scope to the whole file, not just the function
-    if gameBoard[0] == gameBoard[1] == gameBoard[2] and gameBoard[0] != "-":
-        winner = gameBoard[0]
+    if game_board[0] == game_board[1] == game_board[2] and game_board[0] != "-":
+        winner = game_board[0]
         return True
-    elif gameBoard[3] == gameBoard[4] == gameBoard[5] and gameBoard[3] != "-":
-        winner = gameBoard[3]
+    elif game_board[3] == game_board[4] == game_board[5] and game_board[3] != "-":
+        winner = game_board[3]
         return True
-    elif gameBoard[6] == gameBoard[7] == gameBoard[8] and gameBoard[6] != "-":
-        winner = gameBoard[6]
+    elif game_board[6] == game_board[7] == game_board[8] and game_board[6] != "-":
+        winner = game_board[6]
         return True
     
 # Checking for row win or tie   
-def checkVertical(gameBoard):
+def check_vertical(game_board):
     global winner # Global will change the scope to the whole file, not just the function
-    if gameBoard[0] == gameBoard[3] == gameBoard[6] and gameBoard[0] != "-":
-        winner = gameBoard[0]
+    if game_board[0] == game_board[3] == game_board[6] and game_board[0] != "-":
+        winner = game_board[0]
         return True
-    elif gameBoard[1] == gameBoard[4] == gameBoard[7] and gameBoard[1] != "-":
-        winner = gameBoard[2]
+    elif game_board[1] == game_board[4] == game_board[7] and game_board[1] != "-":
+        winner = game_board[2]
         return True
-    elif gameBoard[2] == gameBoard[5] == gameBoard[8] and gameBoard[2] != "-":
-        winner = gameBoard[2]
+    elif game_board[2] == game_board[5] == game_board[8] and game_board[2] != "-":
+        winner = game_board[2]
         return True
 
 # Checking for diagnaol win or tie   
-def checkDiagonal(gameBoard):
+def check_diagonal(game_board):
     global winner # Global will change the scope to the whole file, not just the function
-    if gameBoard[0] == gameBoard[4] == gameBoard[8] and gameBoard[0] != "-":
-        winner = gameBoard[0]
+    if game_board[0] == game_board[4] == game_board[8] and game_board[0] != "-":
+        winner = game_board[0]
         return True
-    elif gameBoard[2] == gameBoard[4] == gameBoard[6] and gameBoard[2] != "-":
-        winner = gameBoard[2]
+    elif game_board[2] == game_board[4] == game_board[6] and game_board[2] != "-":
+        winner = game_board[2]
         return True
 
 # Checking for a tie game, only need to check to see if there is not a blank space   
-def checkTieGame(gameBoard):
-    global gameRunning
-    if "-" not in gameBoard and not checkDiagonal(gameBoard) and not checkHorizontal(gameBoard) and not checkVertical(gameBoard):
-        printGameBoard(gameBoard)
+def check_tie_game(game_board):
+    global game_running
+    if "-" not in game_board and not check_diagonal(game_board) and not check_horizontal(game_board) and not check_vertical(game_board):
+        print_game_board(game_board)
         print("It's a time game!")
-        gameRunning = False
+        game_running = False
 
 # Checking for the winner
-def checkWhoWon():
-    global gameRunning
-    if checkDiagonal(gameBoard) or checkHorizontal(gameBoard) or checkVertical(gameBoard):
-        printGameBoard(gameBoard)
+def check_who_won():
+    global game_running
+    if check_diagonal(game_board) or check_horizontal(game_board) or check_vertical(game_board):
+        print_game_board(game_board)
         print(f"Congratualtions, The winner is player {winner}!")
-        gameRunning = False
+        game_running = False
 
 # Create a function that switches between each players turn
-def switchPlayer():
-    global currentPlayer
-    if currentPlayer == "X":
-        currentPlayer = "O"
+def switch_player():
+    global current_player
+    if current_player == "X":
+        current_player = "O"
     else:
-        currentPlayer = "X"
+        current_player = "X"
 
-def playerComputer(gameBoard):
-    while currentPlayer == "O":
+def player_computer(game_board):
+    while current_player == "O":
         for i in range(9):
-            if gameBoard[i] == "-":
+            if game_board[i] == "-":
                 # Simulate making a move at blank positions for computer player
-                gameBoard[i] = "O"
+                game_board[i] = "O"
                 # This if statement will check if there is any winning moves for the computer player
-                if checkDiagonal(gameBoard) or checkHorizontal(gameBoard) or checkVertical(gameBoard):
-                    switchPlayer()
+                if check_diagonal(game_board) or check_horizontal(game_board) or check_vertical(game_board):
+                    switch_player()
                     return
                 # If it's not a winning move, return it back to a blank position
-                gameBoard[i] = "-"
+                game_board[i] = "-"
         # This range will looks for positions to block player1 from using
         for i in range(9):
-            if gameBoard[i] == "-":
+            if game_board[i] == "-":
                 # Simulate making a move at the position for the player
-                gameBoard[i] = "X"
-                if checkDiagonal(gameBoard) or checkHorizontal(gameBoard) or checkVertical(gameBoard):
-                    gameBoard[i] = "O"
-                    switchPlayer()
+                game_board[i] = "X"
+                if check_diagonal(game_board) or check_horizontal(game_board) or check_vertical(game_board):
+                    game_board[i] = "O"
+                    switch_player()
                     return
-                gameBoard[i] = "-"
+                game_board[i] = "-"
 
         # If there is no winning or blocking move, make a random move
         position = random.randint(0, 8)
-        if gameBoard[position] == "-":
-            gameBoard[position] = "O"
-            switchPlayer()
+        if game_board[position] == "-":
+            game_board[position] = "O"
+            switch_player()
 
-while gameRunning:
-    printGameBoard(gameBoard)
-    playerInput(gameBoard)
-    checkWhoWon()
-    checkTieGame(gameBoard)
-    if not gameRunning:
+while game_running:
+    print_game_board(game_board)
+    player_input(game_board)
+    check_who_won()
+    check_tie_game(game_board)
+    if not game_running:
         break
-    switchPlayer()
-    playerComputer(gameBoard)
-    checkWhoWon()
-    checkTieGame(gameBoard)
+    switch_player()
+    player_computer(game_board)
+    check_who_won()
+    check_tie_game(game_board)
